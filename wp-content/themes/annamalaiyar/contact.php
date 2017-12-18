de1f2dfdac6d162418678e8980130c77c2641b73
tamilrajas <tamilraja@ideas2it.com>
2017-12-18 08:56:44 +0530

    Features: Contact us form changes

--- wp-content/themes/annamalaiyar/contact.php
+++ wp-content/themes/annamalaiyar/contact.php
@@ -4,15 +4,16 @@
 
 			<main>
 					<!-- Welcome Content Section  -->
-	        <section class="bg-primary">
-	            <div class="container center767">
+	        <section class="bg-primary bg-alt">
+	            <div class="container container-padding center767">
+									<h2><?=get_the_title();?></h2>
 									<?php
 										// $page = get_page_by_title( 'Contact Us' );
 										// $content = apply_filters('the_content', $page->post_content);
 										// echo $content;
 									?>
+									<?php echo do_shortcode( '[pirate_forms]' ) ?>
 	            </div>
-							<?php echo do_shortcode( '[pirate_forms]' ) ?>
 	        </section>
 
 	    </main>
